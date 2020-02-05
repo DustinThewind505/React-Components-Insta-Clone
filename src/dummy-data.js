274b27a12e2b34c2cf2d49a8b14c8429f9170a04
Dustin G <42304513+DustinThewind505@users.noreply.github.com>
2020-02-04 19:46:24 -0700

    New dummy data

--- src/dummy-data.js
+++ src/dummy-data.js
@@ -1,5 +1,34 @@
 const dummyData = [
     {
+        username: "Lambda School",
+        thumbnailUrl: 'https://assets-global.website-files.com/5cd091cfb5499f22bdf72905/5e1230986a42a4d4965e22f6_icon.png',
+        imageUrl: 'https://assets-global.website-files.com/5cd091cfb5499f22bdf72905/5e1178ef946b12aa26b5d485_home-photo.jpg',
+        likes: 547698,
+        timestamp: "July 17th 2017, 12:42:40 pm",
+        comments: [
+            {
+                username: "babyJoker138",
+                text: "I'm thinking about joining!"
+            },
+            {
+                username: "Malie420",
+                text: "This is the future right here."
+            },
+            {
+                username: "Ellen_Degenerous",
+                text: "I can't wait to post on the #hired channel!"
+            },
+            {
+                username: "XxXSeinfeldXxX",
+                text: "These are some smart people."
+            },
+            {
+                username: "FoxAndHound",
+                text: "Working remote is the future!"
+            },
+        ]
+    },
+    {
         username: "philzcoffee",
         thumbnailUrl: 'https://instagram.fsjo9-1.fna.fbcdn.net/vp/d4728e507fe1eb2895fb7ca394bb6431/5E4122B1/t51.2885-15/s150x150/71118783_2434459423505735_6295784028176668008_n.jpg?_nc_ht=instagram.fsjo9-1.fna.fbcdn.net',
         imageUrl: 'https://images.unsplash.com/photo-1477763858572-cda7deaa9bc5?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1876&q=80',
