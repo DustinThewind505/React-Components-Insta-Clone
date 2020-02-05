ed7d24768c21e0fce510b71f9dab92e51e0d1bf3
Dustin G <42304513+DustinThewind505@users.noreply.github.com>
2020-02-04 21:05:55 -0700

    Update LikeSection.js

--- src/components/PostsContainer/LikeSection.js
+++ src/components/PostsContainer/LikeSection.js
@@ -3,15 +3,20 @@ import React from 'react';
 const LikeSection = props => {
   return (
     <div>
-    <div className="like-section" key="likes-icons-container">
+    <div
+      className="like-section"
+      key="likes-icons-container"
+    >
       <div className="like-section-wrapper">
-        <i className="far fa-heart" onClick = {props.likePost} />
+        <i className="far fa-heart" onClick={props.add}/>
       </div>
       <div className="like-section-wrapper">
         <i className="far fa-comment" />
       </div>
     </div>
-    <p className="like-number">{props.likes} likes</p>
+    <p className="like-number">
+      
+    {props.likes} likes</p>
 </div>
   )
 };
